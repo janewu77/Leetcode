6578034d9e849d7df0c5462696978074e06dca82
Jane wu <conchwu@hotmail.com>
2022-12-06 12:57:52 +0800

    N746

--- src/main/java/LeetCode/N746EMinCostClimbingStairs.java
+++ src/main/java/LeetCode/N746EMinCostClimbingStairs.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 /**
  * You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
  * You can either start from the step with index 0, or the step with index 1.
@@ -50,13 +52,43 @@ public class N746EMinCostClimbingStairs {
         System.out.println( "["+(result == expected)+"]"+title+".expected "+expected+"; result:"+ result);
     }
 
+    //3.DP bottom-up constant space
+    //Runtime: 0ms, 100%; Memory: 41.6MB, 97.33%
+    //Time:O(N); Space: O(1)
+    public int minCostClimbingStairs(int[] cost) {
+        int cost0 = cost[0], cost1 = cost[1], cost2;
+        for (int i = 2; i < cost.length; i++) {
+            cost2 = cost[i] + Math.min(cost0, cost1);
+            cost0 = cost1; cost1 = cost2;
+        }
+        return Math.min(cost0, cost1);
+    }
+    
+    //2.DP top-down
+    //Runtime: 2ms, 33.42%; Memory: 43.7MB, 45.75%
+    //Time:O(N); Space: O(N)
+    public int minCostClimbingStairs_2(int[] cost) {
+        int[] memo = new int[cost.length + 1];
+        Arrays.fill(memo, -1);
+        return helper_dp(cost, cost.length, memo);
+    }
+    private int helper_dp(int[] cost, int idx, int[] memo){
+        if (idx < 2) return cost[idx];
+        if (memo[idx] != -1) return memo[idx];
+        int res = 0;
+        if (idx < cost.length) res = cost[idx];
+        memo[idx] = res + Math.min(helper_dp(cost, idx - 2, memo), helper_dp(cost, idx - 1, memo));
+        return memo[idx];
+    }
+
+    //1.DP bottom-up in place
     //Runtime: 0 ms, faster than 100.00% of Java online submissions for Min Cost Climbing Stairs.
     //Memory Usage: 41.8 MB, less than 93.59% of Java online submissions for Min Cost Climbing Stairs.
-    //Time complexity:O(N); Space complexity: in place, O(zero)
-    public int minCostClimbingStairs(int[] cost) {
+    //Time:O(N); Space: O(1)
+    public int minCostClimbingStairs_1(int[] cost) {
         for (int i = 2; i < cost.length; i++)
-            cost[i] += Math.min(cost[i-2],cost[i-1]);
-        return Math.min(cost[cost.length-2],cost[cost.length-1]);
+            cost[i] += Math.min(cost[i - 2], cost[i - 1]);
+        return Math.min(cost[cost.length - 2], cost[cost.length - 1]);
     }
 
 }
