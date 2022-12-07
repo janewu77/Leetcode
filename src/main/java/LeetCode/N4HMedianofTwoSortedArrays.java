37cd276dbb52b843e6b1a737f025228820140490
Jane wu <conchwu@hotmail.com>
2022-12-07 23:03:54 +0800

    add binary search

--- src/main/java/LeetCode/N4HMedianofTwoSortedArrays.java
+++ src/main/java/LeetCode/N4HMedianofTwoSortedArrays.java
@@ -56,33 +56,74 @@ public class N4HMedianofTwoSortedArrays {
         System.out.println("[" + (expected == res) +"] "+(c++)+ ".result:"+ res + ".expected:"+expected);
     }
 
+
+    //2.Binary Search
+    //Runtime: 2ms, 100%; Memory: 43.4MB, 86.4%
+    //Time: O(log(N + M)); Space: O(1)
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length == 0 && nums2.length == 0) return 0;
+        int n = nums1.length, m = nums2.length;
+        if (n > m) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            n = nums1.length;
+            m = nums2.length;
+        }
+
+        int left = 0, right = n;
+        int halfCount = (n + m + 1) >> 1;
+
+        // Binary Search
+        while (left < right) {
+            int i = (left + right + 1) >> 1;
+            int j = halfCount - i;
+
+            if (nums1[i - 1] > nums2[j]) right = i - 1;
+            else left = i;
+        }
+
+        //median
+        int i = left, j = halfCount - i;
+        int leftMax = Math.max(i == 0 ? Integer.MIN_VALUE : nums1[i - 1], j == 0 ? Integer.MIN_VALUE : nums2[j - 1]);
+        if ((n + m) % 2 == 1) return leftMax;
+
+        int rightMin = Math.min(i == n ? Integer.MAX_VALUE : nums1[i], j == m ? Integer.MAX_VALUE : nums2[j]);
+        return (leftMax + rightMin) / 2.0;
+    }
+
+
+    //1.merge
+    //TLE
     //Runtime: 2 ms, faster than 100.00% of Java online submissions for Median of Two Sorted Arrays.
     //Memory Usage: 43.1 MB, less than 95.12% of Java online submissions for Median of Two Sorted Arrays.
-    //merge nums1+nums2
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    //Time: O(M + N); Space: O(1)
+    public double findMedianSortedArrays_1(int[] nums1, int[] nums2) {
         if (nums1.length == 0 && nums2.length == 0) return 0;
 
         int k = (nums1.length + nums2.length) >> 1;
-        int i = 0; int j = 0;
-        int sum = 0;
-        int pre = 0;
+        int i = 0, j = 0;
+        int median1 = 0, median2 = 0;
+
         while (i + j <= k ){
-            pre = sum;
-            if (i == nums1.length){
+            median1 = median2;
+            if (i == nums1.length) {
                 j = k - i;
-                if (j - 1 >= 0) pre = Math.max(pre, nums2[j - 1]);
-                sum = nums2[j++];
-            }else if (j == nums2.length){
+                if (j - 1 >= 0) median1 = Math.max(median1, nums2[j - 1]);
+                median2 = nums2[j++];
+            } else if (j == nums2.length) {
                 i = k - j;
-                if (i - 1 >= 0) pre = Math.max(pre, nums1[i - 1]);
-                sum = nums1[i++];
-            }else
-                sum = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+                if (i - 1 >= 0) median1 = Math.max(median1, nums1[i - 1]);
+                median2 = nums1[i++];
+            } else {
+                //merge
+                median2 = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+            }
         }
 
         //even
-        if (((nums1.length + nums2.length) & 1) == 0) return (0d + pre + sum) / 2;
-        return sum;
+        if (((nums1.length + nums2.length) & 1) == 0) return (0d + median1 + median2) / 2;
+        return median2;
     }
 
 }
