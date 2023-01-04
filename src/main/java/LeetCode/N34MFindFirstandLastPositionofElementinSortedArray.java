087f55bc3378311c2fb9e83a981109b96535fd0d
Jane wu <conchwu@hotmail.com>
2023-01-04 18:19:09 +0800

    binary search

--- src/main/java/LeetCode/N34MFindFirstandLastPositionofElementinSortedArray.java
+++ src/main/java/LeetCode/N34MFindFirstandLastPositionofElementinSortedArray.java
@@ -46,33 +46,75 @@ public class N34MFindFirstandLastPositionofElementinSortedArray {
 
     public static void main(String[] args) {
         int[] data;
-        String expected;
         data = new int[]{9,9,9,9,9,9,9,9,9,9};
-        expected = "0,9";
-        doRun("0,9", data, 9);
+        doRun(new int[]{0, 9}, data, 9);
 
         data = new int[]{5,7,7,8,8,10};
-        expected = "3,4";
-        doRun(expected, data, 8);
+        doRun(new int[]{3, 4}, data, 8);
 
         data = new int[]{1,2,2};
-        expected = "1,2";
-        doRun(expected, data, 2);
+        doRun(new int[]{1, 2}, data, 2);
 
+        doRun(new int[]{0, 0}, new int[]{1,3}, 1);
+        doRun(new int[]{1, 1}, new int[]{1,3}, 3);
     }
 
     private static int c = 1;
-    private static void doRun(String expected, int[] nums, int target){
+    private static void doRun(int[] expected, int[] nums, int target){
         int[] res = new N34MFindFirstandLastPositionofElementinSortedArray().searchRange(nums, target);
-        String tmp = Arrays.stream(res).mapToObj(i -> String.valueOf(i)).collect(Collectors.joining(","));
-        System.out.println("[" + (expected.equals(tmp)) +"] "+(c++)
-                + ".[result:"+ tmp + "][expected:"+expected+"]");
+        System.out.println("[" + Arrays.equals(expected, res) +"].[result:"+ Arrays.toString(res) + "][expected:"+Arrays.toString(expected)+"]");
+    }
+
+    //2. two binary searches
+    //Runtime: 0ms 100%; Memory: 47.4MB 32%
+    //Time: O(logN); Space: O(1)
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[]{-1, -1};
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid] && (mid == 0 || nums[mid - 1] != target)) {
+                res[0] = mid; break;
+            }
+            if (nums[mid] >= target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        left = 0; right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid] && (mid == nums.length - 1 || nums[mid + 1] != target)) {
+                res[1] = mid; break;
+            }
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return res;
+    }
+
+
+    //1.one pass
+    //Runtime: 2ms 12%; Memory: 46.5MB 58%
+    //Time: O(N); Space: O(1)
+    public int[] searchRange_1(int[] nums, int target) {
+        int[] res = new int[]{-1, -1};
+        if (nums.length <= 0) return res;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (i == 0 || nums[i - 1] != target) res[0] = i;
+                if (i == nums.length - 1 || nums[i + 1] != target) res[1] = i;
+            }
+        }
+        return res;
     }
 
     //Runtime: 0 ms, faster than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
     //Memory Usage: 45.6 MB, less than 90.63% of Java online submissions for Find First and Last Position of Element in Sorted Array.
     //time: o(log(n) * 2* log(n/2)
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange_11(int[] nums, int target) {
         int[] res = new int[]{-1, -1};
         if (nums.length <= 0) return res;
         if (nums[0] == target && nums[nums.length - 1] == target) return new int[]{0, nums.length - 1};
